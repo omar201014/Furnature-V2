b7a573656f7788929e4dfa59ddd8c49f696a92fb
omar201014 <omar_hussein201014@hotmail.com>
2022-04-09 00:50:47 +0200

    click-overlay added

--- app.js
+++ app.js
@@ -1,52 +1,16 @@
     
-    const contentArr = Array.from(document.getElementsByClassName('product-cont'));
-    const overlay = document.querySelectorAll('.overlay-hov');
-    const buttonHov = document.querySelectorAll('.btn-hov')
+    const cards = document.getElementById('cards')
+    
 
-    function clickHoveron(){   
-
-    for (content of contentArr){
-        this.addEventListener('click' , function(){ 
-            // this.removeChild(this.children[1])            
-            overlay.forEach(item =>{                
-            if(item.style.display ==='none'){
-                item.style.display ='block'
-            }  
-             else{
-                item.style.display ='block' 
-             }                       
-            })
-
-            buttonHov.forEach(button =>{
-                button.classList.remove('mt-5' , 'btn-lg')
-            }) 
-            
-        })                                 
-                                                                
-    }
-};
-
-
-function clickHoveroff(){
-    overlay.forEach(div => {
-        div.addEventListener('click' , function(){
-            if (div.style.display ==='block'){
-                delete div.style.display['block']                
-            }
-            
-            console.log(div);
-        })
+    cards.addEventListener('click' , (event)=>{
+        event.target.closest('figure').lastElementChild.classList.toggle('overlay-click')
     })
-}
 
 
 
 
 
+    
 
-
-
-document.addEventListener("DOMContentLoaded" , clickHoveron)
-document.addEventListener("DOMContentLoaded" , clickHoveroff)
 
 
